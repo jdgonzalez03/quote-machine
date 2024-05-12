317124bdac626385a778a3901307e979ad04d98a
jdgonzalez03 <juandavidgonzalezurrego@gmail.com>
2024-05-11 22:26:00 -0500

    some constants

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,12 @@
 export const base_url =
   "https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json";
+
+export const transition = "all 1s";
+
+export function generateColors() {
+  const blue = Math.floor(Math.random() * 128);
+  const green = Math.floor(Math.random() * 128);
+  const red = Math.floor(Math.random() * 128);
+
+  return `rgb(${red},${green},${blue})`;
+}

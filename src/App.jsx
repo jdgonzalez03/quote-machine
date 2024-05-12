3953ae56dce00f02f9ac94f07f7faa1d816932f9
jdgonzalez03 <juandavidgonzalezurrego@gmail.com>
2024-05-11 22:25:50 -0500

    add color interactivity and the new quotes button

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,69 @@
-import { useState } from 'react'
-import './App.css'
-import { Button } from './components/Button'
-import { useEffect } from 'react';
-import { base_url } from './constants';
+import { useState } from "react";
+import "./App.css";
+import { Button } from "./components/Button";
+import { useEffect } from "react";
+import { base_url, generateColors, transition } from "./constants";
 
 function App() {
-
   const [quotes, setQuotes] = useState([]);
-  const [quote, setQuote] = useState({})
+  const [quote, setQuote] = useState({});
+  const [randomColor, setRandomColor] = useState(generateColors());
 
-  useEffect(()=>{
+  useEffect(() => {
     fetch(base_url)
-      .then(response => response.json())
-      .then(data =>{ 
-        setQuotes(data)
+      .then((response) => response.json())
+      .then((data) => {
+        setQuotes(data);
 
         //On first load display a random quote
         const randomIndex = Math.floor(Math.random() * data.length);
         setQuote(data[randomIndex]);
       })
-      .catch(error => console.error('Error getting quotes', error))
-  }, [])
+      .catch((error) => console.error("Error getting quotes", error));
+  }, []);
 
   const newQuote = () => {
-    const index =  Math.floor(Math.random() * (quotes.length - 0) + 0);
-    setQuote(quotes[index])
-  }
+    const index = Math.floor(Math.random() * (quotes.length - 0) + 0);
+    setQuote(quotes[index]);
+    setRandomColor(generateColors());
+  };
 
   return (
     <>
-      <header>
-        <h1>Random Quote Machine</h1>
-      </header>
-      <section id='quote-box'>
-        <h2 id='text'>{quote.quote}</h2>
-        <aside>
-          <p id='author'>{quote.author}</p>
-        </aside>
-        <aside>
-          <a id='tweet-quote' href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(`"${quote.quote}" - ${quote.author}`)}`} target='_blank'>
-            Tweet this quote
-          </a>
-          <Button id='new-quote' onClick={newQuote}>Next Quote</Button>
-        </aside>
-      </section>
-      <footer>By <span>jdgonzalez</span></footer>
+      <div
+        className="app"
+        style={{
+          backgroundColor: randomColor,
+          transition,
+        }}
+      >
+        <section id="quote-box">
+          <h2 id="text">{quote.quote}</h2>
+          <aside>
+            <p id="author">{quote.author}</p>
+          </aside>
+          <aside className="buttons">
+            <a
+              id="tweet-quote"
+              className="btn"
+              href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(
+                `"${quote.quote}" - ${quote.author}`
+              )}`}
+              target="_blank"
+            >
+              Tweet this quote
+            </a>
+            <Button id="new-quote" className="btn" onClick={newQuote}>
+              Next Quote
+            </Button>
+          </aside>
+        </section>
+        <footer>
+          By <span>jdgonzalez</span>
+        </footer>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
